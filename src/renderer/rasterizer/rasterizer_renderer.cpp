00ce7f2d0e9d5df3a61b36e407fd84de76cc66c1
RamPrin <l.zelenskiy@innopolis.university>
2022-06-20 16:22:37 +0300

    Revert "Rasterization homework finished"
    This reverts commit e5535ab773e8632e3d1fe3983a21d79707e92e06.

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -8,7 +8,7 @@ void cg::renderer::rasterization_renderer::init()
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(settings->width, settings->height);
 	rasterizer = std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
 	rasterizer->set_viewport(settings->width, settings->height);
-
+	rasterizer->set_render_target(render_target);
 
 	model = std::make_shared<cg::world::model>();
 	model->load_obj(settings->model_path);
@@ -27,9 +27,7 @@ void cg::renderer::rasterization_renderer::init()
 	camera->set_angle_of_view(settings->camera_angle_of_view);
 	camera->set_z_near(settings->camera_z_near);
 	camera->set_z_far(settings->camera_z_far);
-
-	depth_buffer = std::make_shared<cg::resource<float>>(settings->width, settings->height);
-	rasterizer->set_render_target(render_target, depth_buffer);
+	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
 }
 void cg::renderer::rasterization_renderer::render()
 {

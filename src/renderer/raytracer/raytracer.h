4b7afa26a286676e31256e0fa6d7191c6224f546
RamPrin <l.zelenskiy@innopolis.university>
2022-06-20 17:06:33 +0300

    Lab 2.01. Implement ray_generation and trace_ray method of raytracer class

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -186,7 +186,12 @@ namespace cg::renderer
 	inline payload raytracer<VB, RT>::trace_ray(
 			const ray& ray, size_t depth, float max_t, float min_t) const
 	{
-		// TODO: Lab 2.01. Implement ray_generation and trace_ray method of raytracer class
+		if(depth == 0)
+			return miss_shader(ray);
+
+		depth--;
+
+		return miss_shader(ray);
 		// TODO: Lab 2.02. Adjust trace_ray method of raytracer class to traverse geometry and call a closest hit shader
 		// TODO: Lab 2.04. Adjust `trace_ray` method of `raytracer` to use `any_hit_shader`
 		// TODO: Lab 2.05. Adjust trace_ray method of raytracer class to traverse the acceleration structure
